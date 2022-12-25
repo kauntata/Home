dd9f83ce47900e8e34ae737c2bab14034b5ec69c
unknown <kauntata@mail.ru>
2022-12-25 19:44:42 +0400

    first commit 2

--- jk/Program.cs
+++ jk/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите число от 100 до 999");
+int n = int.Parse(Console.ReadLine()!);
+int c = 0;
+if (n > 99)
+{
+    if(n < 999)
+    {
+        c = n%10;
+     System.Console.WriteLine(c);
+    }
+    else
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else System.Console.WriteLine("Третьей цифры нет");
