c635496d87c1fe2b88e4017b1a4679b1ef850fad
unknown <kauntata@mail.ru>
2022-12-25 19:37:05 +0400

    first commit 1

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число от 100 до 999");
+int n = int.Parse(Console.ReadLine()!);
+int b = (n/10)%10;
+Console.Write (b);
