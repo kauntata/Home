cc3d2aee799ad6b8171a0e25ff76d2dc628d388d
unknown <kauntata@mail.ru>
2022-12-25 19:47:04 +0400

    first commit 3

---
+++ rt/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine()!);
+if(n > 5)
+{
+   if(n > 5);
+   {
+   System.Console.WriteLine("да");
+   }
+}
+
+else System.Console.WriteLine("нет");
