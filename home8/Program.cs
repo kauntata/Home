0d7b30def75a22470d97e1c2cafb5d042c89c09f
unknown <kauntata@mail.ru>
2022-12-28 17:35:34 +0400

    first commit dz8

---
+++ home8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ввидите n");
+int n = int.Parse(Console.ReadLine()!);
+for(int i = 1; i <= n; i++)
+{
+if (i == n){Console.Write($"{Math.Pow(i, 3)}");}
+else Console.Write($"{Math.Pow(i, 3)}, ");
+
+}
