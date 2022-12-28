426f96d7dc38ab3dd5286d7fa7ad18586a3f1b44
unknown <kauntata@mail.ru>
2022-12-28 17:37:45 +0400

    first commit dz10

---
+++ home010/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int n = int.Parse(Console.ReadLine()!);
+int c= ((n%10)*10);
+int a = ((n%100)/10);
+int b = (n/1000);
+int b1 = (a + c);
+if(10000<n)
+{
+    if (n<100000)
+    {
+       if(b == b1)
+       {
+        Console.Write ($" Число является палиндромом ");
+       }
+        else Console.Write ($" Число не является палиндромом ");
+    }
+    else  System.Console.WriteLine("Введено неверно  значение");
+}
+else  System.Console.WriteLine("Введено неверно  значение");
+
