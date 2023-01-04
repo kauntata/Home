3b06bdee5b8a81f216ab206628d6f3504e3286f3
unknown <kauntata@mail.ru>
2023-01-04 22:36:21 +0400

    homedz1

---
+++ home011/Program.cs
@@ -0,0 +1,10 @@
+﻿int [] array =new int[8];
+Random random = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 99);
+}
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
