5e8221d278cd95a79a6444ca3cef935772f1a47b
unknown <kauntata@mail.ru>
2023-01-04 22:37:18 +0400

    homedz2

---
+++ home012/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите два числа A");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите два числа B");
+int B = int.Parse(Console.ReadLine()!);
+int C = 1;
+for(int i = B; i > 0; i--)
+{
+    C = (C*A);
+} 
+System.Console.WriteLine(C);
